f4f7890e421be479bcfe98141f8327f9e2dc3569
cjjknight <christopher.johnson42@gmail.com>
2024-08-01 17:00:29 -0500

    Added Scooters Coffee

--- IceCreamLocator/StoreListView.swift
+++ IceCreamLocator/StoreListView.swift
@@ -14,8 +14,9 @@ struct StoreListView: View {
         IceCreamStore(name: "Happy Ice Cream", address: "5025 Church Rd STE 125, Olive Branch, MS 38654", phone: "(662) 370-9848", latitude: 34.9614, longitude: -89.8295),
         IceCreamStore(name: "Baskin-Robbins", address: "4805 Bethel Rd, Olive Branch, MS 38654", phone: "(662) 890-9216", latitude: 34.9638, longitude: -89.8387),
         IceCreamStore(name: "Frutanas", address: "1609, 9113 MS-178 suite c, Olive Branch, MS 38654", phone: "", latitude: 34.9650, longitude: -89.8353),
-        IceCreamStore(name: "Braum's Ice Cream & Dairy Stores", address: "6431 Goodman Rd, Olive Branch, MS 38654", phone: "(662) 890-9429", latitude: 34.9620, longitude: -89.8252)
-    ]
+        IceCreamStore(name: "Braum's Ice Cream & Dairy Stores", address: "6431 Goodman Rd, Olive Branch, MS 38654", phone: "(662) 890-9429", latitude: 34.9620, longitude: -89.8252),
+        IceCreamStore(name: "Scooters Coffee", address: "Cockrum and College Street, Olive Branch, MS", phone: "(555) 555-5555", latitude: 34.9543, longitude: -89.8302)
+]
     
     var body: some View {
         NavigationView {
